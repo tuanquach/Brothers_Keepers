bd0cdb8babfc882938f18e779d3eb1f5fe014b2d
vinh quach <vinh quach@DESKTOP-VEDUP1S>
2016-12-17 00:07:01 -0500

    add skills

---
+++ BrothersKeepers/Models/Skills.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BrothersKeepers.Models
+{
+    public class Skills
+    {
+        public int SkillsId { get; set; }
+        public string SkillsName { get; set; }
+        public string Description { get; set; }
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+
+    }
+}
